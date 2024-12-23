34ca9df125c0a311d3051a7fd5bbf7b62933fa2b
Yuvashree C <cyuvashree171@gmail.com>
2024-12-24 00:51:30 +0530

    Add files via upload

---
+++ FindOddNumbers.java
@@ -0,0 +1,23 @@
+package week2.day2;
+
+public class FindOddNumbers {
+
+	public static void main (String[] args) {
+		
+		for (int i=0; i<=10; i++)
+		{
+			if(i%2!=0) {
+				System.out.println("the odd num"+i);
+			}
+		}
+	}
+}
+
+
+
+
+
+	
+
+
+
